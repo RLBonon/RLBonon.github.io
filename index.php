2c86419feb99b6d7742259cf5a0bc47801178afa
Ltakeshima <rbonon28@gmail.com>
2023-04-28 16:55:48 +0800

    Fixing output alignment

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;}
+p    {color: red;}
+textarea {
+    display: block;
+    margin-left: 150;
+    margin-right: 150;
+}
+</style>
 <?php
 $json="" ;
 $reversed_text="" ;
@@ -6,11 +16,11 @@ if(isset($_POST['submit'])) {
   $text = $_POST['text'];
   $json = file_get_contents($_POST['text']);
   $obj = json_decode($json);
-
-  foreach($obj as $key=>$value){
-    echo $key . " => " . $value . "<br>";
-	echo strrev($key);
-	}
+#Fixing output alignment
+#  foreach($obj as $key=>$value){
+ #   echo $key . " => " . $value . "<br>";
+#	echo strrev($key);
+#	}
 	#Reverse text
   $reversed_text = strrev($json);
 }
@@ -18,6 +28,7 @@ if(isset($_POST['submit'])) {
 
 <form method="post" action="">
 <label for="URLorg" class="form-label">URL:</label>
+
 <textarea name="text" rows="4" cols="50"></textarea>
   <br>
   <button type="submit" name="submit" style="background-color:Chartreuse;">Query</button>
@@ -28,5 +39,5 @@ if(isset($_POST['submit'])) {
   <br>
   <label for="InvertUrl" class="form-label">INVERTED URL RESPONSE:</label>
   <textarea name="reversed_text" rows="4" cols="50"><?php echo $reversed_text; ?></textarea>
-  
+
 </form>
